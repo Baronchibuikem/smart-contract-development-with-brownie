7ea4988ed8aed1a1f4d314d92cffc443af777207
baron <baronchibuike@gmail.com>
2022-09-17 20:26:21 +0100

    added contract for creating zombies

---
+++ contracts/CreateZombies.sol
@@ -0,0 +1,27 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    // declare our event here
+    event NewZombieCreated(uint id, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        emit NewZombieCreated(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+}
--- scripts/util_scripts.py
+++ scripts/util_scripts.py
@@ -2,6 +2,7 @@ from brownie import network, config, accounts
 
 
 def get_account():
+    # running this locally, this will spin up an address and a private key for test purposes
     if(network.show_active() == "development"):
         return accounts[0]
     else:
